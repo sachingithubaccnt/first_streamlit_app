b3c6d28bca8ca62f30375c401498108e30619f63
sachingithubaccnt <59443683+sachingithubaccnt@users.noreply.github.com>
2023-06-16 14:54:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Avocado'])
-streamlit.dataframe(my_fruit_list)
+##We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
+##streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Avocado'])
+#streamlit.dataframe(my_fruit_list)
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Avocado'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
