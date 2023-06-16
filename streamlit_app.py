fefecc6b8ed0d7394223181ebe2f51399fd79fd0
sachingithubaccnt <59443683+sachingithubaccnt@users.noreply.github.com>
2023-06-16 14:44:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('my first app')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacoda Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
